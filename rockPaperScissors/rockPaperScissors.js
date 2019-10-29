3aba00fc2991ea32330e12e0b409037e9df26a2f
liu-austin <austinliu279@gmail.com>
2019-10-29 09:41:08 -0700

    incomplete solution for rockPaperScissors

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -1,3 +1,4 @@
+// jshint esversion:6
 /*
 * Write a function that generates every sequence of throws a single
 * player could throw over a three-round game of rock-paper-scissors.
@@ -17,9 +18,27 @@
 * rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...]
 *
 */
+let actions = ['R', 'P', 'S'];
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function(num) {
   // TODO: your solution here
+    let solutionsArr = [];
+
+    let roundsPlayed = 0;
+    let solutionsArrSize = Math.pow(3, num);
+    for (let i = 0; i < solutionsArrSize; i++) {
+        solutionsArr[i] += actions[(i % 3)];
+    }
+    for (let j = 0; i < solutionsArrSize / (3 * roundsPlayed); j++) {
+        for (let k of actions) {
+            // solutionsArr[j * ]
+        }
+    }
+    
+};
+
+var pushRecursive = function(term) {
+    let sequence = '';
+
 };
 

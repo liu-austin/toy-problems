fea6156cd657c66b221a80aac3d66660c253bd12
liu-austin <austinliu279@gmail.com>
2019-11-07 10:29:24 -0800

    completes quicksort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -1,3 +1,4 @@
+// jshint esversion:6
 /**
  * Quicksort is a sorting algorithm that uses a divide and conquer strategy;
  *
@@ -11,5 +12,79 @@
  */
 
 
-var quicksort = function(array) {
-};
+// var quicksort = function(array) {
+//     let left = [];
+//     let right = [];
+//     let midpointIndex = Math.floor((array.length - 1) / 2);
+//     let pivot = array[midpointIndex];
+//     let sorted = [];
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (i !== midpointIndex) {
+//             if (array[i] <= pivot) {
+//                 left.push(array[i]);
+//             } else {
+//                 right.push(array[i]);
+//             }
+//         }
+//     }
+//     if (left.length === 1) {
+//         sorted.push(left[0]);
+//     } else if (left.length !== 0) {
+//         sorted.concat(quicksort(left));
+//     }
+
+//     sorted.push(pivot);
+
+//     if (right.length === 1) {
+//         sorted.push(right[0]);
+//     } else if (right.length !== 0) {
+//         sorted.concat(quicksort(right));
+//     }
+
+//     return sorted;
+
+// };
+
+// a = quicksort([1,5,2,4,8,3,-1]);
+
+
+function partition(array, left, right) {
+    let pivot = array[Math.floor((right + left) / 2)];
+    let i = left;
+    let j = right;
+    while (i <= j) {
+        while (array[i] < pivot) {
+            i++;
+        }
+        while (array[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(array, i, j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quicksort(array, left=0, right=array.length - 1) {
+    var index;
+    if (array.length > 1) {
+        index = partition(array, left, right); 
+        if (left < index - 1) {
+            quicksort(array, left, index - 1);
+        }
+        if (index < right) { 
+            quicksort(array, index, right);
+        }
+    }
+    return array;
+}
+
+function swap(array, leftIndex, rightIndex){
+    var temp = array[leftIndex];
+    array[leftIndex] = array[rightIndex];
+    array[rightIndex] = temp;
+}

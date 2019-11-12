70cc094e078b9ed4c5db711fde92df21565b3a22
liu-austin <austinliu279@gmail.com>
2019-11-12 09:36:01 -0800

    completes nonrepeating characters

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -1,3 +1,4 @@
+// jshint esversion:6
 /**
  * Given an arbitrary input string, return the first nonrepeated character in
  * the string. For example:
@@ -8,4 +9,20 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  let chars = {};
+  let key = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (!chars[string[i]]) {
+      chars[string[i]] = {i, occ: 1};
+    } else {
+      chars[string[i]].occ += 1;
+    }
+  }
+  let nonRepeating = Object.keys(chars).filter(char => {
+    return chars[char].occ === 1;
+  });
+  let firstNonRepeatedCharacter = nonRepeating.sort((a, b) => {
+    return chars[a].i - chars[b].i;
+  })[0];
+  return firstNonRepeatedCharacter;
 };

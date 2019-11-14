7f699d0a836e7ec29bfb6f33c117444ddb98bf3b
liu-austin <austinliu279@gmail.com>
2019-11-14 09:47:14 -0800

    completes anagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -1,8 +1,10 @@
+// jshint esversion:6
 /**
  * Given a single input string, write a function that produces all possible anagrams
  * of a string and outputs them as an array. At first, don't worry about
  * repeated strings.  What time complexity is your solution?
  *
+ * TIME COMPLEXITY: 
  * Extra credit: Deduplicate your return array without using uniq().
  */
 
@@ -12,6 +14,39 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
+var allAnagrams = function(string, index=0, inputArr=[]) {
   // Your code here.
+  if (string.length < 2) {
+    return [string];
+  }
+  let outputArr = [];
+  if (index === string.length) {
+    return inputArr;
+  }
+  if (index === 0) {
+    for (let i = 0; i < string.length; i++) {
+      outputArr.push(string[i]);
+    }
+  } else {
+    for (let i = 0; i < inputArr.length; i++) {
+      for (let j = 0; j < string.length; j++) {
+        if (!inputArr[i].includes(string[j])) {
+          outputArr.push(inputArr[i] + string[j]);
+        }
+      }
+    }
+  }
+  return allAnagrams(string, index + 1, outputArr);
 };
+
+// var allAnagrams = function(string, obj={}, inputArr=[]) {
+//   let outputArr = [];
+//   if (Object.keys(obj).length === 0) {
+//     for (let i = 0; i < string.length; i++) {
+//       outputArr.push(string[i]);
+//     }
+//   }
+//   for (let i = 0; i < inputArr.length; i++) {
+    
+//   }
+// };

fc0aa77dd1b740c5e950cddb722fd23fb4473335
liu-austin <austinliu279@gmail.com>
2019-11-20 09:34:34 -0800

    completes binary search array

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -1,3 +1,4 @@
+// jshint esversion:6
 /*
  * Given a sorted array, find the index of an element
  * using a binary search algorithm.
@@ -10,6 +11,20 @@
  * console.log(index); // null
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function (array, target, lowerIndex=0, upperIndex=array.length-1) {
+    if (upperIndex === lowerIndex && array[upperIndex] !== target) {
+        return null;
+    }
+    let midpoint = Math.floor((lowerIndex + upperIndex) / 2);
+    if (array[midpoint] === target) {
+        return midpoint;
+    } else if (array[midpoint] > target) {
+        upperIndex = midpoint - 1;
+        return binarySearch(array, target, lowerIndex, upperIndex);
+    } else {
+        lowerIndex = midpoint + 1;
+        return binarySearch(array, target, lowerIndex, upperIndex);
+    }
 };
 
+// TIME COMPLEXITY: LOG(N) where N is array size

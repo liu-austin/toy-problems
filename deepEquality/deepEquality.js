2b06e982865ff9a35cb9b8ae6b28a60cfaf824f3
liu-austin <austinliu279@gmail.com>
2019-11-22 09:40:35 -0800

    completes deepEquality

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -1,3 +1,4 @@
+// jshint esversion:6
 /**
   * Write a function that, given two objects, returns whether or not the two
   * are deeply equivalent--meaning the structure of the two objects is the
@@ -11,5 +12,31 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(apple, orange) {
+var deepEquals = function(apple, orange, i=0) {
+  var changed = false;
+  var deepEqual;
+  if (Object.keys(apple).length !== Object.keys(orange).length) {
+    return false;
+  }
+  for (let i = 0; i < Object.keys(apple).length; i++) {
+    if (!Object.keys(orange).includes(Object.keys(apple)[i])) {
+      return false;
+    }
+  }
+  for (let j in apple) {
+    if (typeof(apple[j]) !== 'object' && apple[j] === orange[j]) {
+      continue;
+    } else if (typeof(apple[j]) === 'object') {
+      deepEqual = deepEquals(apple[j], orange[j], i + 1);
+        changed = true;
+    } else {
+      return false;
+    }
+  }
+  if (changed) {
+    return deepEqual;
+  } else {
+    return true;
+  }
+
 };
